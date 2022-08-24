e03e812afadaab8693a7d9acda0a967aaf606fe4
Italo Maurício <105960758+italomauricio1@users.noreply.github.com>
2022-08-24 14:54:09 -0300

    Update

--- main.c
+++ main.c
@@ -18,20 +18,55 @@ MÓDULO RELATÓRIO
 #include <unistd.h>
 
 
-char telaPrincipal();
 
 
-char telaPrincipal(){
+char telaInicial();
+
+
+char main(void)
+
+ {
 
     system("clear||cls"); 
     printf("| ---------------------------------------------------------------------- |\n");
     printf("| ----- // UNIVERSIDADE FEDERAL DO RIO GRANDE DO NORTE - CERES // ------ |\n");
+    printf("| -------- // Departamento de Computacao e Informatica // ---------------|\n");
+    printf("| ---------------------------------------------------------------------- |\n");                                       
+    printf("|               Disciplina: DCT1106 - PROGRAMACAO - 2022.2               |\n");
+    printf("|          Projeto: Sistema de Controle de Despensa Domestica            |\n");
+    printf("|            Colaboradores:  Manuelly Victor e Italo Mauricio            |\n"); 
+    printf("|                  Docente: Dr Flavius Gorgonio                          |\n");
+    printf("|                                                                        |\n");
     printf("| ---------------------------------------------------------------------- |\n");
-    prinft("|               Disciplina: DCT1106 - PROGRAMAÇãO - 2022.2               |\n");
-    prinft("|          Projeto: Sistema de Controle de Despensa Doméstica            |\n");
-    printf("|          Colaboradores:  Manuelly Victor e Italo Mauricio              |\n"); 
-    printf("|          Docente: Dr Flavius Gorgonio                                  |\n");
-
+    printf("| ---------------------- SIG - Pranty - 2022.2 ------------------------- |\n");
+    printf("\t\t\t>>> Tecle <ENTER> para continuar...\n");
+    getchar();
     return "c";
 
+    
+
 }
+
+
+char telaPrincipal(void)
+
+{
+
+    system("clear||cls"); 
+    printf("| ====================================================================== |\n");
+    printf("| ====                      SIG - Pranty                            ==== |\n");
+    printf("| ====================================================================== |\n");
+    printf("| ---------------------------------------------------------------------- |\n");
+    printf("|                                                                        |\n");
+    printf("|                      1 - Cadastrar Produtos                            |\n");
+    printf("|                      2 -                                               |\n"); 
+    printf("|                      3 -                                               |\n");
+    printf("|                      4 - Sair                                          |\n");
+    printf("|                                                                        |\n");
+    printf("| ---------------------------------------------------------------------- |\n");
+    printf("| ====================================================================== |\n");
+    getchar();
+
+
+
+} 
