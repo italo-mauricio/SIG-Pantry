9661591fa0b8c6b970150af67076884abfa983db
Italo Maurício <105960758+italomauricio1@users.noreply.github.com>
2022-08-24 11:11:09 -0300

    Create main.c

---
+++ main.c
@@ -0,0 +1,37 @@
+/*
+Para a primeira semana deve-se fazer: 
+Crie algumas telas com informações básicas sobre o seu projeto. O código que exibe essas telas deve estar em 
+uma função diferente da função main() e deve ser chamada a partir desta. 
+MÓDULO PRINCIPAL (NOME DO PROJETO, DISCIPLINA...)
+MÓDULO SOBRE O QUE SE TRATA O PROJETO (RESUMO + EQUIPE)
+MÓDULO PARA CADASTRAR OS ITENS EXISTENTES NA CASA POR CATEGORIA (ALIMENTOS, HIGIENTE PESSOAL E LIMPEZA)
+MÓDULO PARA LISTAR OS ITENS QUE ESTÃO EM FALTA POR CATEGORIA
+MÓDULO PARA EXIBIR OS ITENS QUE ESTÃO PRÓXIMOS DO PRAZO DE VENCIMENTO
+MÓDULOS DOS ITENS COMPRADOS (ATUALIZAÇÃO)
+MÓDULO RELATÓRIO
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+#include <unistd.h>
+
+int main(void) {
+
+    system("clear||cls"); 
+    printf("| ---------------------------------------------------------------------- |\n");
+    printf("| ----- // UNIVERSIDADE FEDERAL DO RIO GRANDE DO NORTE - CERES // ------ |\n");
+    printf("| ---------------------------------------------------------------------- |\n");
+    prinft("|               Disciplina: DCT1106 - PROGRAMAÇãO - 2022.2               |\n");
+    prinft("|          Projeto: Sistema de Controle de Despensa Doméstica            |\n");
+    printf("|          Colaboradores:  Manuelly Victor e Italo Mauricio              |\n"); 
+                                                  ")
+   
+    printf("<<<<<<<<<<<        Projeto: SIG-Pantry            >>>>>>>>>\n");
+
+
+    system("pause");
+    return 0;
+
+}
