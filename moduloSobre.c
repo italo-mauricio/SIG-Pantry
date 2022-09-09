75fa2b4e5664e0bf5cffd05fe4ffeba7f77b17f2
Manuelly Rodrigues Victor <manuellyvictor2000@gmail.com>
2022-09-09 12:48:45 -0300

    modularizando

---
+++ moduloSobre.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "moduloSobre.h"
+
+
+void telaSobre ( void )
+{
+    system ( " clear||cls " );
+    printf ( " | ------------------------------------------------------------------ | \n " );
+    printf ( " | ---- // UNIVERSIDADE FEDERAL DO RIO GRANDE DO NORTE - CERES // --- | \n " );
+    printf ( " | -------- // Departamento de Computação e Informática // ---------- | \n " );
+    printf ( " | ------------------------------------------------------------------ | \n " );
+    printf ( " |                                                                    | \n " );                                       
+    printf ( " |            Disciplina: DCT1106 - PROGRAMAÇÃO - 2022.2              | \n " );
+    printf ( " |         Projeto: Sistema de Controle de Despensa Doméstica         | \n " );
+    printf ( " |                    Docente: Dr Flavius Gorgonio                    | \n " );
+    printf ( " |                                                                    | \n " );
+    printf ( " |  Este programa consiste no controle das atividades de uma despensa | \n " );
+    printf ( " |    doméstica, sendo dividida em categorias no intuito de ajudar na | \n " );
+    printf ( " |                organização das atividades cotidianas.              | \n " );
+    printf ( " |             Equipe: Manuelly Rodrigues e Italo Mauricio            | \n " );
+    printf ( " |                                                                    | \n " );
+    printf ( " | ------------------------------------------------------------------ | \n " );
+    printf ( " | ---------------------- SIG - Pranty - 2022.2 --------------------- | \n " );
+    printf ("\t\t\t>>> Tecle <ENTER> para continuar...                              | \n " );
+    getchar();
+
+}
