3bcb67282dc9e155d020bf36c3d66403b3a46035
Manuelly Rodrigues Victor <manuellyvictor2000@gmail.com>
2022-09-09 12:50:10 -0300

    modularizando

--- SigPantry.c
+++ SigPantry.c
@@ -4,11 +4,10 @@
 #include "modulocadastroItem.h"
 #include "modulolistadeCompras.h"
 #include "moduloRelatorio.h"
-
+#include "moduloSobre.h"
 
 // Assinatura das funções
 char  telaPrincipal (void);
-void  telaSobre (void);
 
 char telaPrincipal(void)
 {
@@ -37,26 +36,3 @@ char telaPrincipal(void)
 
 }
 
-void  telaSobre ( void )
-{
-    system ( " clear||cls " );
-    printf ( " | ------------------------------------------------------------------ | \n " );
-    printf ( " | ---- // UNIVERSIDADE FEDERAL DO RIO GRANDE DO NORTE - CERES // --- | \n " );
-    printf ( " | -------- // Departamento de Computação e Informática // ---------- | \n " );
-    printf ( " | ------------------------------------------------------------------ | \n " );
-    printf ( " |                                                                    | \n " );                                       
-    printf ( " |            Disciplina: DCT1106 - PROGRAMAÇÃO - 2022.2              | \n " );
-    printf ( " |         Projeto: Sistema de Controle de Despensa Doméstica         | \n " );
-    printf ( " |                    Docente: Dr Flavius Gorgonio                    | \n " );
-    printf ( " |                                                                    | \n " );
-    printf ( " |  Este programa consiste no controle das atividades de uma despensa | \n " );
-    printf ( " |    doméstica, sendo dividida em categorias no intuito de ajudar na | \n " );
-    printf ( " |                organização das atividades cotidianas.              | \n " );
-    printf ( " |             Equipe: Manuelly Rodrigues e Italo Mauricio            | \n " );
-    printf ( " |                                                                    | \n " );
-    printf ( " | ------------------------------------------------------------------ | \n " );
-    printf ( " | ---------------------- SIG - Pranty - 2022.2 --------------------- | \n " );
-    printf ("\t\t\t>>> Tecle <ENTER> para continuar...                              | \n " );
-    getchar();
-
-}
