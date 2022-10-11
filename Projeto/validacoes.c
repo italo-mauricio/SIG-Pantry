fbed036a2bd6e596596f242ab0c2c8a46d598cf9
Manuelly Rodrigues Victor <107930253+Manuelly1@users.noreply.github.com>
2022-10-11 20:18:50 -0300

    criação do módulo de validações

---
+++ Projeto/validacoes.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "valiacoes.h"
+
+int lerEmail(void)
+{
+/* o que é um e-mail invalido? 
+
+Tem mais de um '@' ou nenhum
+Tem somente numerais ou nada nos extremos de '@'
+Tem 1 ou mais dots (.) em sequência ou seguido de '@'
+Tem parte local [antes do '@'] de tamanho maior que 64 caracteres
+Tem espaço "   "
+Não tem caixa de entrada. */
+
+
+
+}
+
+
+int lerCPF(void)
+{
+
+
+}
+
+
+int lerData(void)
+{
+
+
+}
+
+int lerString(void)
+{
+
+
+}
+
+int lerCodigoBarras(void)
+{
+
+
+}
