eb9b41318b904a2a77eb6544ecd7753a6a2916e5
italo <italogithub@gmail.com>
2022-10-13 12:13:08 -0300

    adicionando email

--- Projeto/validacoes.c
+++ Projeto/validacoes.c
@@ -42,3 +42,42 @@ int lerCodigoBarras(void)
 
 
 }
+
+int lerEmail(void){
+
+  char email[50]={"abc@xyz.com"};
+  int tam=strlen(email);
+  int arroba = 0, ponto = 0, antesPonto = 0, depoisPonto = 0, i;
+
+  for (i = 0; i < tam; i++) {
+    char c = email[i];
+    if(c == '@') {
+      if (arroba)
+        break; // não pode ter uma segunda @
+      arroba = 1;
+      if (i < 3)
+        break; // se @ vier antes de 3 caracteres, erro
+    }
+    else if (arroba) { // se já encontrou @
+      if (ponto) { // se já encontrou . depois de @
+        depoisPonto++;
+      }
+      else if(c == '.') {
+        ponto = 1;
+        if (antesPonto < 3) {
+          break; // se . depois de @ vier antes de 3 caracteres, erro
+        }
+      }
+      else {
+        antesPonto++;
+      }
+    }
+  } // for
+
+    if (i == tam && depoisPonto > 1)
+        printf("Valido");
+    else
+        printf("Invalido");
+
+
+}
