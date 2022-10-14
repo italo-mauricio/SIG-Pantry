ee4ba10357672bdb3143ad423ff8900b4f084198
Manuelly Rodrigues Victor <107930253+Manuelly1@users.noreply.github.com>
2022-10-13 22:38:00 -0300

    função de validação de nome

--- Projeto/validacoes.c
+++ Projeto/validacoes.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "valiacoes.h"
 
-int lerEmail(void)
+/*int lerEmail(void)
 {
 /* o que é um e-mail invalido? 
 
@@ -11,7 +12,7 @@ Tem somente numerais ou nada nos extremos de '@'
 Tem 1 ou mais dots (.) em sequência ou seguido de '@'
 Tem parte local [antes do '@'] de tamanho maior que 64 caracteres
 Tem espaço "   "
-Não tem caixa de entrada. */
+Não tem caixa de entrada. 
 
 
 
@@ -31,14 +32,47 @@ int lerData(void)
 
 }
 
-int lerString(void)
-{
-
-
-}
 
 int lerCodigoBarras(void)
 {
 
 
+} */
+
+
+int lerNome(void)
+{
+    char tamanho;
+    char i;
+    char nome [20];
+    tamanho = strlen(nome);
+    if (tamanho > 1) {
+        return 0;
+    }
+        for (int i = 0; i < tamanho; i++) {
+            if (nome[i] == 'a' && nome[i] <= 'z') {
+                return 0;
+            }
+            if (nome[i] == 'A' && nome[i] <= 'Z') {
+                return 0;
+            } 
+            if (nome[i] == ' ') {
+                return 0;  
+            }         
+            if (nome[i] == '.') {
+                return 1;
+            }
+            if (nome[i] == ',') {
+                return 1;
+            }
+            if (nome[i] == '/') {
+                return 1;
+            }
+            if (nome[i] == ';') {
+                return 1;
+            }
+            else {
+                return 1;  
+            }          
+        }  
 }
