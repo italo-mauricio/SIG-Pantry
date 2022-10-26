31667fc75e8fa94adb2a440d2918f4f3bcb47a54
Manuelly Rodrigues Victor <107930253+Manuelly1@users.noreply.github.com>
2022-10-26 12:26:28 -0300

    adicionando struct

--- Projeto/moduloEntradaItens.h
+++ Projeto/moduloEntradaItens.h
@@ -2,3 +2,19 @@ void menuEntradaItens(void);
 char telaEntradaItens(void);
 void telaRegistrarEntrada(void);
 void telaCancelarEntrada(void);
+
+typedef struct entrada Entrada;
+struct entrada
+{
+    char codigodeBarras [15];
+    char quantProduto [13];
+};
+
+typedef struct cancelarent CancelarEnt;
+struct cancelarent
+{
+    char codigodeBarras [15];
+};
+
+Entrada* infoEntrada( );
+CancelarEnt* infoCancelarEntrada( );
