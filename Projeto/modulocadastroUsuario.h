1796f085bd14ab31d99af6640b25e4566033dc6d
Manuelly Rodrigues Victor <107930253+Manuelly1@users.noreply.github.com>
2022-11-12 20:50:45 -0300

    organizando

--- Projeto/modulocadastroUsuario.h
+++ Projeto/modulocadastroUsuario.h
@@ -11,44 +11,6 @@ struct usuario {
     char status;
 };
 
-//struct para excluir algum dado do usuário
-typedef struct excluirUsuario ExcluirUsuario;
-struct excluirUsuario
-{
-    char email [40];
-};
-
-//structs para atualização de algum parâmetro
-typedef struct atualizarNome AtualizarNome;
-struct atualizarNome
-{
-    char nome [40];
-};
-
-typedef struct atualizarEmail AtualizarEmail;
-struct atualizarEmail
-{
-    char email [40];
-};
-
-typedef struct atualizarDataNasc AtualizarDataNasc;
-struct atualizarDataNasc
-{
-    int dia, mes, ano;
-}; 
-
-typedef struct atualizarUsername AtualizarUsername;
-struct atualizarUsername
-{
-    char username[20];
-};
-
-typedef struct atualizarSenha AtualizarSenha;
-struct atualizarSenha
-{
-    char senha[20];
-};
-
 void modulocadastroUsuario(void);
 int telaMenuUsuario(void);
 void telaAtualizarUsuario(void);
@@ -58,6 +20,4 @@ void gravaUsuario(Usuario*);
 void listaInfoUsuario(void);
 void exibeInfoUsuario(Usuario* cliente);
 void buscaInfoUsuario(void);
-
-//ALTERAR:
 void infoExcluirUs(void);
