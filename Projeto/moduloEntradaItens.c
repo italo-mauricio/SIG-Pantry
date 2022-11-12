92b74866e03263811ff5468568c5381377a3493d
Manuelly Rodrigues Victor <107930253+Manuelly1@users.noreply.github.com>
2022-11-12 09:48:41 -0300

    crud (mudando funções e adicionando busca + lista)

--- Projeto/moduloEntradaItens.c
+++ Projeto/moduloEntradaItens.c
@@ -6,38 +6,47 @@
 
 void menuEntradaItens(void)
 {
-    Entrada* regEntrada;
-    CancelarEnt* cancEntrada;
     char escolha;
     do {
-        escolha = telaEntradaItens();
+        escolha = telaRegistrarEntrada();
         switch(escolha) {
-            case '1':
-                regEntrada = infoEntrada();
-                gravaEntrada(regEntrada);
-                free(regEntrada);
-                break;
-            case '2':
-                cancEntrada = infoCancelarEntrada();
-                gravaCancelamentoEnt(cancEntrada);
-                free(cancEntrada);
-                break;
-        } 
+        case '1':
+            infoEntrada(); //informações do item
+            break;
+        case '2':
+            pesquisarEntrada(); //pesquisa
+            break;
+        case '3':
+            atualizarEntrada(); //edição
+            break;
+        case '4':
+            excluirEntrada(); //exclusão
+            break;
+        case '5':
+            listarEntrada(); //relatório
+            break;      
+        default:
+            printf("Opção inválida\n");
+            break;
+        }
      
     } while (escolha != '0');
 
 }
 
-char telaEntradaItens(void)
+char telaRegistrarEntrada(void)
 {
     char esc;
     system ( " clear||cls " );
     printf(" | ========================================================= | \n");
     printf(" | --------------------------------------------------------- | \n");
-    printf(" | ------ SIG-Pantry - ENTRADA DE ITENS DA DESPENSA -------- | \n");
+    printf(" | -------- REGISTRAR ENTRADA DE ITENS NA DESPENSA --------- | \n");
     printf(" |                                                           | \n");
-    printf(" |                 1- Registrar entrada                      | \n");
-    printf(" |                 2- Cancelar entrada                       | \n");                 
+    printf(" |                 1- Cadastrar entrada                      | \n");
+    printf(" |                 2- Pesquisar entrada                      | \n");
+    printf(" |                 3- Editar entrada                         | \n");                 
+    printf(" |                 4- Excluir entrada                        | \n");
+    printf(" |                 5- Listar entrada                         | \n");                                                                    
     printf(" |                 0- Voltar à tela principal                | \n");
     printf(" |                                                           | \n");
     printf(" | ========================================================= | \n");
@@ -49,43 +58,49 @@ char telaEntradaItens(void)
 
 }
 
-//Função para entrada de um item ao estoque
-Entrada* infoEntrada(void)
+//Função para cadastrar a entrada de um item ao estoque
+void infoEntrada(void)
 {
     Entrada* ent;
-    ent = (Entrada*)malloc(sizeof(Entrada));
-    system ( " clear||cls " );
+    system ( " cls " );
     printf(" | ========================================================= | \n");
     printf(" | --------------------------------------------------------- | \n");
     printf(" | -------------- REGISTRAR ENTRADA DE ITEM ---------------- | \n");
     printf(" |                                                           | \n");    
-
+    ent = (Entrada*)malloc(sizeof(Entrada));
     do
     {
-        printf(" | Informe o código de barras: ");
+        printf(" | Informe o código de barras do item: ");
         scanf("%s", ent->codigodeBarras);
         getchar();
         
     } while(!lerQuantidade(ent->codigodeBarras));
 
-    do
-    {
-        printf(" | Informe a quantidade de produto: ");
-        scanf("%s", ent->quantProduto);
+    do {        
+        printf(" | Informe o dia de vencimento: ");
+        scanf("%d",&ent->dia);
+        getchar();
+        printf(" | Informe o mês: ");
+        scanf("%d",&ent->mes);
+        getchar();
+        printf(" | Informe o ano: ");
+        scanf("%d",&ent->ano);
         getchar();
         
-    } while(!lerQuantidade(ent->quantProduto));
+     } while(!valida_data(ent->dia, ent->mes, ent->ano));  
     
     printf(" |                                                           | \n");
     printf(" | ========================================================= | \n");
     printf(" | Press ENTER for exit... ");
+    ent->status = '1'; //o 1 mostra que foi cadastrado
+    gravaEntrada(ent);
+    free(ent);
+    printf(" | Pressione qualquer tecla para sair.... ");
     getchar();
-    ent->status = '1'; //o true mostra que foi cadastrado
-    return ent;
 
 }
 
-//Função para gravar no arquivo:
+//Função para gravar no arquivo
 void gravaEntrada(Entrada* ent) 
 {
     FILE* fp;
@@ -100,45 +115,139 @@ void gravaEntrada(Entrada* ent)
     
 }
 
-//Função para procurar o produto que deseja fazer o cancelamento
-CancelarEnt* infoCancelarEntrada(void)
+//A partir do código de barras
+void buscaInfoEntrada(void)
 {
-    CancelarEnt* canc;
-    canc = (CancelarEnt*) malloc(sizeof(CancelarEnt));  
-    system ( " clear||cls " );
+    FILE* fp;
+    Entrada* ent;
+    int achou;
+    char procurado[15];
+    fp = fopen("entrada.dat", "rb");
+
+    if (fp == NULL) {
+        printf("Ops! Erro na abertura do arquivo!\n");
+        exit(1);
+    }
+    printf("\n\n");
+    system ( " cls " );
     printf(" | ========================================================= | \n");
     printf(" | --------------------------------------------------------- | \n");
-    printf(" | --------------- CANCELAR ENTRADA DE ITEM ---------------- | \n");
-    printf(" |                                                           | \n");  
-    do
-    {
-        printf(" | Informe o código de barras: ");
-        scanf("%s", canc->codigoBarras);
-        getchar();
-        
-    } while (!lerQuantidade(canc->codigoBarras));
+    printf(" |                   Buscar dados da entrada                 | \n");
+    printf(" | ========================================================= | \n");
+    printf("Informe o código de barras: ");
+    scanf(" %30[^\n]", procurado);
+    getchar();
+    ent = (Entrada*) malloc(sizeof(Entrada));
+    achou = 0;
+    while((!achou) && (fread(ent, sizeof(Entrada), 1, fp))) {
+        printf("Código de barras |%s|\n", ent->codigodeBarras);
+        if ((strcmp(ent->codigodeBarras, procurado) == 0) && (ent->status == '1')) {
+            achou = 1;
+        }
+    }
+    fclose(fp);
+    if (achou) {
+        exibeEntrada(ent);
+    } else {
+        printf("Os dados da entrada %s não foram encontrados\n", procurado);
+    }
+    free(ent);
+    printf(" | Pressione qualquer tecla para sair.... ");
+    getchar();
     
+}
+
+//exibe produtos registrados na entrada
+void exibeEntrada(Entrada* ent) {
+    printf(" | Código de barras: %s\n", ent->codigodeBarras);    
+    printf(" | Quantidade do produto: %s\n", ent->quantProduto);
+    printf(" | Dia do vencimento: %d\n", ent->dia); 
+    printf(" | Mês do vencimento: %d\n", ent->mes); 
+    printf(" | Ano do vencimento: %d\n", ent->ano); 
+    printf(" | Status: %c\n", ent->status);
     printf(" |                                                           | \n");
     printf(" | ========================================================= | \n");
-    printf(" | Press ENTER for exit... ");
+    printf(" | Pressione qualquer tecla para sair.... ");
     getchar();
-    canc->status = '1'; 
-    return canc;
+    
+}
 
+//EDITAR AQUI
+
+
+//função para exclusão lógica
+void excluirEntrada(void)
+{
+    FILE* fp;
+    Entrada* ent;
+    int achou;
+    char resp;
+    char codigodeBarras[20];
+    fp = fopen("entrada.dat", "r+b");
+
+    if (fp == NULL){
+        printf("Ops! Erro na abertura do arquivo!\n");
+        exit(1);
+    }
+    ent = (Entrada*) malloc(sizeof(Entrada));
+    system( " clear || cls ");
+    printf(" | ============================================================== | \n");
+    printf(" | -------------------------------------------------------------- | \n");
+    printf(" | ---------------------- EXCLUIR ENTRADA ----------------------- | \n");
+    printf(" |                                                                | \n");
+    printf(" | Informe o código de barras do produto que você deseja excluir: ");
+    scanf("%s", ent->codigodeBarras);
+    getchar();  
+    achou = 0;
+    while ((!achou) && (fread(ent, sizeof(Entrada), 1, fp))){
+        if ((strcmp(ent->codigodeBarras, codigodeBarras) == 0) && (ent->status == '0')){
+            achou = 1;
+        }
+    }
+
+   if (achou){
+        exibeEntrada(ent);
+        getchar();
+        printf("Deseja realmente excluir os dados desta entrada? (s/n)");
+        scanf("%c", &resp);
+        if (resp == 's' || resp == 'S'){
+            ent->status = '0';
+            fseek(fp, (-1)*sizeof(Entrada), SEEK_CUR);
+            fwrite(ent, sizeof(Entrada), 1, fp);
+            printf("\nDados da entrada excluídos com sucesso!");
+        }else{
+            printf("\nTudo bem, os dados não foram alterados!");
+        }
+    }else{
+        printf("Os dados não foram encontrados!");
+    }
+    free(ent);
+    fclose(fp);
+    printf(" | Pressione qualquer tecla para sair.... ");
+    getchar();
+    
 }
 
 
-//Função para gravar no arquivo:
-void gravaCancelamentoEnt(CancelarEnt* canc) 
+//listar entradas
+void listarEntrada(void) 
 {
     FILE* fp;
-    fp = fopen("cancentrada.dat", "ab");
+    Entrada* ent;
+    fp = fopen("entrada.dat", "rb");
     if (fp == NULL) {
-        printf("Ops! Ocorreu um erro na abertura do arquivo!\n");
+        printf("Ops! Erro na abertura do arquivo!\n");
         exit(1);
     }
-    
-    fwrite(canc, sizeof(CancelarEnt), 1, fp);
+    printf("\n\n");
+    printf(" | ==================== Exibe entrada ====================== | \n");
+    printf(" |                                                           | \n");
+    printf(" | ========================================================= | \n");
+    ent = (Entrada*)malloc(sizeof(Entrada));
+    while(fread(ent, sizeof(Entrada), 1, fp)) {
+        exibeEntrada(ent);
+    }
     fclose(fp);
-    
+    free(ent);
+
 }
