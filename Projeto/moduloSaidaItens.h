618a74ffe8856aca97fbf0c3f2aa1e2a06011f9c
italo <italogithub@gmail.com>
2022-12-04 01:31:23 -0300

    update

--- Projeto/moduloSaidaItens.h
+++ Projeto/moduloSaidaItens.h
@@ -1,9 +1,9 @@
 //assinatura das funções do arquivo .c
 void menuSaidaItens(void);
 char telaRegistrarSaida(void);
-void infoSaida(void);
+int infoSaida(void);
 int listarSaidas(void);
 int buscainfoSaida(void);
-int telaAtualizarSaida(void);
+//int telaAtualizarSaida(void);
 char escAtualizarSaida(void);
-int excluirSaida(void);
+//int excluirSaida(void);
